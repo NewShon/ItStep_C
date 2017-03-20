cd0218db0fa231beee69e2559b91a08f163a65ef
NewShon <kirill31_95@mail.ru>
2017-03-20 19:21:24 +0300

    Create Dynamic_stack.cpp

---
+++ 27_Dynamic_stack/Dynamic_stack.cpp
@@ -0,0 +1,22 @@
+struct LIFO
+{
+	char data;
+	LIFO *prev;
+};
+LIFO *push(LIFO *top, char ch)//первый вариант написания функции
+{
+	LIFO *cur = (LIFO*)malloc(sizeof(LIFO));
+	cur->data = ch;
+	cur->prev = top;
+	top = cur;
+	return top;
+}
+char pop(LIFO **top)//второй вариант написания функции
+{
+	char temp;
+	LIFO *cur = *top;
+	if (*top == NULL) return 0;
+	temp = (*top)->data;
+	free(cur);
+	return temp;
+}
