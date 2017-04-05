564f8042a3b7f569917c2ce7f0cf50e8b32a6772
NewShon <kirill31_95@mail.ru>
2017-04-05 21:07:18 +0300

    Create ex1,2.cpp

---
+++ 32_Bit_operations/ex1,2.cpp
@@ -0,0 +1,47 @@
+//Даны 2 числа и операция. Вывести на консоль выбранный  байт ответа. 
+//Например: 21*37=777, 777= 00000011 00001001, последний байт это 00001001=9.
+//Дано число, вывести  его двоичное представление, используя только битовые операции.
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+
+
+int main()
+{
+
+	int a, b;
+	cout << "Input a,b: "; cin >> a >> b;
+	cout << "*/+-: ";
+	char symb;
+	cin >> symb;
+	int result;
+	switch (symb){
+	case '*': result = a*b; break;
+	case '/': result = a/b; break;
+	case '+': result = a+b; break;
+	case '-': result = a-b; break;
+	}
+	for (int i = 31; i >= 0; --i)
+	{
+		cout << ((result >> i) & 1);//побитовый сдвиг на единицу и битовое & с единицей для получения каждого отдельного бита
+		if (i % 8 == 0) cout << " ";
+	}
+	int number; cout << "\nInput number of byte (0-3): "; cin >> number;
+	int new_result = ((255 << (3 - number) * 8)&result) >> ((3 - number) * 8);
+	//(3 - number) * 8) - выбор на сколько сдвинуть 255 (0-8-16-24)
+	//((255 << (3 - number) * 8) - сдвиг 255 для получения нужного байта
+	//((255 << (3 - number) * 8)&result) - к полученому байту применяем битовое & для получения заданного байта
+	//>> ((3 - number) * 8) - сдвиг обратно на (0-8-16-24)битиков)
+
+	cout << endl;
+	for (int i = 7; i >= 0; --i)
+	{
+		cout << ((new_result >> i) & 1);
+	}
+	
+	cout << endl;
+	return 0;
+}
+
